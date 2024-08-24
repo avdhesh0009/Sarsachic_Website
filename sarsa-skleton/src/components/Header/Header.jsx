c1c55ed40c655beb641f7e011d80efae2ff07359
avdhesh0009 <avdheshkumar41237@gmail.com>
2024-08-24 21:28:42 +0530

    profile page fixed

--- sarsa-skleton/src/components/Header/Header.jsx
+++ sarsa-skleton/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useContext } from "react";
 import { FaBars, FaTimes, FaRegUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { TfiSearch } from "react-icons/tfi";
 import { GoHeart, GoArrowUpRight } from "react-icons/go";
 import { BsCart } from "react-icons/bs";
@@ -11,6 +11,8 @@ import assesories from '../../images/assesories.png';
 import caps from '../../images/caps.png';
 import coords from '../../images/coords.png';
 import './Header.css';
+import { WebContext } from "../../providers/WebProvider";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -18,6 +20,11 @@ const Header = () => {
   const [activeCategory, setActiveCategory] = useState('men');
   const [selectedCurrency, setSelectedCurrency] = useState('Currency');
   const selectorRef = useRef(null);
+
+  const navigate  = useNavigate();
+
+  const {user} = useContext(WebContext);
+  console.log(user);
 
   const handleClickOutside = (event) => {
     if (selectorRef.current && !selectorRef.current.contains(event.target)) {
@@ -47,6 +54,14 @@ const Header = () => {
   const handleOptionClick = (currency) => {
     setSelectedCurrency(currency);
     setIsOpen(false);
+  };
+
+  const handleProfile = () => {
+    if (!user) {
+      navigate('/login'); // Programmatically navigate to the login page
+    } else {
+      navigate('/userProfile'); // Programmatically navigate to the user profile page
+    }
   };
 
   return (
@@ -88,7 +103,9 @@ const Header = () => {
         </div>
         <Link to="/mywishlist">  <GoHeart className="icon" /></Link>
         <Link to="/cart">  <BsCart className="icon" /></Link>
-        <Link to="/userProfile">  <FaRegUser className="icon" /></Link>
+        <button onClick={handleProfile}>
+          <FaRegUser className="icon" />
+        </button>
       </div>
 
       {menuOpen && (
