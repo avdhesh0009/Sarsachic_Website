1fe115868b57bc3b7c0d690678ae1ae4a5d2c465
avdhesh0009 <avdheshkumar41237@gmail.com>
2024-09-05 22:06:16 +0530

    cartnew updated

--- skleton-backend/src/controllers/cart.controllers.js
+++ skleton-backend/src/controllers/cart.controllers.js
@@ -2,10 +2,9 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { User } from '../models/user.model.js';
 
-
 const addToCart = asyncHandler(async (req, res) => {
   const { productId, quantity, size } = req.body;
-  console.log(size);
+  
   if (!productId || !quantity) {
     return res.status(400).json({ message: 'Product ID and quantity are required' });
   }
@@ -40,7 +39,8 @@ const addToCart = asyncHandler(async (req, res) => {
 
 
 const removeFromCart = asyncHandler(async (req, res) => {
-  const { productId } = req.params;
+  const { productId } = req.body;
+
   const user = await User.findById(req.user._id);
 
   user.cart = user.cart.filter(item => String(item.product) !== productId);
@@ -54,4 +54,29 @@ const getCart = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, user.cart));
 });
 
-export { addToCart, removeFromCart, getCart };
+const updateCart = asyncHandler(async(req,res)=>{
+  const { productId, sizeId , quantity } = req.body;
+
+   const user = await User.findById(req.user._id).populate("cart.product");
+   if(!user){
+      return res.status(404).json({ message: "User not found" });
+   }
+
+   const cartItem = user.cart.find(item => item.product._id.toString() === productId);
+   if (!cartItem) {
+       return res.status(404).json({ message: "Product not found in cart" });
+   }
+
+   const sizeToUpdate = cartItem.sizes.find(size => size._id.toString() === sizeId);
+   if (!sizeToUpdate) {
+       return res.status(404).json({ message: "Size not found in cart" });
+   }
+
+   sizeToUpdate.quantity = quantity;
+
+   await user.save();
+
+   res.status(200).json({ message: "Cart item size quantity updated to 0", cart: user.cart });
+})
+
+export { addToCart, removeFromCart, getCart,updateCart };
